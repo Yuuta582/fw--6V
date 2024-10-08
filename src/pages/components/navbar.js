f8f51b3bc8ebb0fdb9ddfea8dba4091cb98e565d
Renata Zaurisio <119963435+Yuuta582@users.noreply.github.com>
2024-10-08 18:30:05 +0000

    Aula 8/10

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,4 +1,21 @@
-function Navbar() {
-    return <h1>Navbar</h1>
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function Menu() {
+  return (
+    <Navbar expand="sm" className="degrademenu">
+      <Container>
+        <Navbar.Brand href="/">6V</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
+import Menu from "./components/navbar";
+
 export default function Index() {
-  return <h1>Olá Next</h1>
+  return <>
+  <Menu /> 
+  <h1>Olá Turma 6V</h1>
+  </>
 }
